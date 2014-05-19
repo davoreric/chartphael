3da1828c568e6b3a21c1b9116e8039f2517b566e
Davor Eric <davor.eric@trikoder.com>
2014-05-19 17:04:57 +0200

    js syntax update

--- dist/chartphael.js
+++ dist/chartphael.js
@@ -1,123 +1,144 @@
-var app = app || {};
-app.chart = app.chart || {};
+/************************************************
+*********** Chartphael graph plugin *************
+*************************************************
+based on Raphael.js
+developed by Davor Eric
+https://github.com/davoreric/chartphael
+************************************************/
 
+function chartphael(options) {
 
+    if ( !(document.getElementById(options.id) == null || chartphael[options.type] == null) ) {
 
-app.chart.arc = {
-
-	init: function(id){
-
-		this.el = document.getElementById(id)
-		this.width = this.el.offsetWidth;
-		this.stroke = 6;
-		this.radius = (this.width - 2*this.stroke)/2;
-		this.center = this.radius + this.stroke;
-
-		this.colorBkg = '#ccc';
-		this.colorChart = '#8fbb48';
-
-		this.paper = Raphael(id,this.width,this.width);
-
-		this.setArcAttr();
-		this.setBkg();
-		this.setChart(this.el.getAttribute('data-value'));
-
-	},
-
-	setBkg: function(){
-
-		var bkg = this.paper.circle(this.center, this.center, this.radius)
-							.attr({stroke: this.colorBkg, 'stroke-width': this.stroke});
-
-	},
-
-	setChart: function(end){
-
-		var chart = this.paper.path().attr({
-		    'stroke': this.colorChart,
-		    'stroke-width': this.stroke,
-		    arc: [this.center, this.center, 0, 100, this.radius]
-		});
-
-		chart.animate({
-		    arc: [this.center, this.center, end, 100, this.radius]
-		}, 1500, "bounce");
-
-	},
-
-	setArcAttr: function(){
-
-		this.paper.customAttributes.arc = function (xloc, yloc, value, total, R) {
-		    var alpha = 360 / total * value,
-		        a = (90 - alpha) * Math.PI / 180,
-		        x = xloc + R * Math.cos(a),
-		        y = yloc - R * Math.sin(a),
-		        path;
-		    if (total == value) {
-		        path = [
-		            ["M", xloc, yloc - R],
-		            ["A", R, R, 0, 1, 1, xloc - 0.01, yloc - R]
-		        ];
-		    } else {
-		        path = [
-		            ["M", xloc, yloc - R],
-		            ["A", R, R, 0, +(alpha > 180), 1, x, y]
-		        ];
-		    }
-		    return {
-		        path: path
-		    };
-		};
-
-	}
-
-};
-
-app.chart.pie = {
-
-	init: function(id,data){
-
-		this.el = document.getElementById(id);
-		this.data = data.items;
-		this.width = this.el.offsetWidth;
-		this.height = this.el.offsetHeight;
-
-		this.centerX = this.height/2;
-		this.centerY = this.width/2;
-		this.radius = this.centerX-40;
-
-		this.paper = Raphael(id,this.width,this.height);
-
-		this.setBkg();
-		this.setPie();
-
-	},
-
-	setBkg: function(){
-
-		var bkg = this.paper.circle(this.centerY, this.centerX, this.radius)
-							.attr({fill: this.colorBkg, 'stroke-width': 0});
-
-	},
-
-	setPie: function(){
-
-		var values = [],
-			labels = [],
-			colors = [];
-
-		for(i=0;i<this.data.length;i++){
-			values.push(parseInt(this.data[i].percent));
-        	labels.push(this.data[i].label);
-        	colors.push(this.data[i].color);
-		}
-		
-		this.paper.pieChart(this.centerY, this.centerX, this.radius, values, labels, colors);
+    	switch(options.type){
 			
-	}
+			case "arc":
+	    		new chartphael.arc(options);
+	    		break;
+
+	    	case "pie":
+	    		new chartphael.pie(options);
+	    		break;
+
+	    	case "line":
+	    		new chartphael.line(options);
+	    		break;
+
+		}
+
+		return this;
+
+    }
+
+}
+
+/************************************************
+*********** Arc graph ***************************
+************************************************/
+
+chartphael.arc = function(options) {
+
+	this.node = document.getElementById(options.id);
+	this.width = this.node.offsetWidth;
+	this.stroke = 6;
+	this.radius = (this.width - 2*this.stroke)/2;
+	this.center = this.radius + this.stroke;
+	this.colorBkg = '#ccc';
+	this.colorChart = '#8fbb48';
+
+	this.paper = Raphael(options.id,this.width,this.width);
+
+	this.setArcAttr();
+	this.setBkg();
+	this.setChart(this.node.getAttribute('data-value'));
+
+}
+
+chartphael.arc.prototype.setBkg = function(){
+
+	var bkg = this.paper.circle(this.center, this.center, this.radius).attr({stroke: this.colorBkg, 'stroke-width': this.stroke});
 
 };
 
+chartphael.arc.prototype.setChart = function(end){
+
+	var chart = this.paper.path().attr({
+	    'stroke': this.colorChart,
+	    'stroke-width': this.stroke,
+	    arc: [this.center, this.center, 0, 100, this.radius]
+	});
+
+	chart.animate({
+	    arc: [this.center, this.center, end, 100, this.radius]
+	}, 1500, "bounce");
+
+};
+
+chartphael.arc.prototype.setArcAttr = function(){
+
+	this.paper.customAttributes.arc = function (xloc, yloc, value, total, R) {
+	    var alpha = 360 / total * value,
+	        a = (90 - alpha) * Math.PI / 180,
+	        x = xloc + R * Math.cos(a),
+	        y = yloc - R * Math.sin(a),
+	        path;
+	    if (total == value) {
+	        path = [
+	            ["M", xloc, yloc - R],
+	            ["A", R, R, 0, 1, 1, xloc - 0.01, yloc - R]
+	        ];
+	    } else {
+	        path = [
+	            ["M", xloc, yloc - R],
+	            ["A", R, R, 0, +(alpha > 180), 1, x, y]
+	        ];
+	    }
+	    return {
+	        path: path
+	    };
+	};
+
+};
+
+
+/************************************************
+*********** Pie graph ***************************
+************************************************/
+
+chartphael.pie = function(options) {
+
+	this.node = document.getElementById(options.id);
+	this.data = options.data.items;
+	this.width = this.node.offsetWidth;
+	this.height = this.node.offsetHeight;
+
+	this.centerX = this.height/2;
+	this.centerY = this.width/2;
+	this.radius = this.centerX-40;
+
+	this.paper = Raphael(options.id,this.width,this.height);
+
+	this.setPie();
+
+}
+
+chartphael.pie.prototype.setPie = function(){
+
+	var values = [],
+		labels = [],
+		colors = [];
+
+	for(i=0;i<this.data.length;i++){
+		values.push(parseInt(this.data[i].percent));
+    	labels.push(this.data[i].label);
+    	colors.push(this.data[i].color);
+	}
+	
+	this.paper.pieChart(this.centerY, this.centerX, this.radius, values, labels, colors);
+		
+};
+
+/* Raphael extend for Pie graph */
 Raphael.fn.pieChart = function (cx, cy, r, values, labels, colors) {
     var paper = this,
         rad = Math.PI / 180,
@@ -169,61 +190,85 @@ Raphael.fn.pieChart = function (cx, cy, r, values, labels, colors) {
 
 };
 
-app.chart.line = {
 
-	init: function(id,data){
+/************************************************
+*********** Line graph **************************
+************************************************/
 
-		this.el = document.getElementById(id);
-		this.data = data;
-		this.width = this.el.offsetWidth;
-		this.height = this.el.offsetHeight;
+chartphael.line = function(options) {
 
-		this.padding = 40;
+	this.node = document.getElementById(options.id);
+	this.data = options.data;
+	this.width = this.node.offsetWidth;
+	this.height = this.node.offsetHeight;
 
-		this.bottom_left = [this.padding,this.height-this.padding];
-		this.bottom_right = [this.width-this.padding,this.height-this.padding];
-		this.top_left = [this.padding,this.padding];
-		this.top_right = [this.width-this.padding,this.padding];
+	this.padding = 40;
 
-		this.paper = Raphael(id,this.width,this.height);
+	this.bottom_left = [this.padding,this.height-this.padding];
+	this.bottom_right = [this.width-this.padding,this.height-this.padding];
+	this.top_left = [this.padding,this.padding];
+	this.top_right = [this.width-this.padding,this.padding];
+	
+	this.gridWidth = this.width - (this.padding*2);
+	this.gridHeight = this.height - (this.padding*2);
 
-		this.x_axis_value = this.data.axis.x;
-		this.y_axis_value = this.data.axis.y;
+	this.x_axis_value = this.data.axis.x;
+	this.y_axis_value = this.data.axis.y;
+	this.incrementX = this.gridWidth/(this.x_axis_value.length-1);
+	this.incrementY = this.gridHeight/(this.y_axis_value.length-1);
 
-		this.setGrid();
-		this.setPoints();
+	this.paper = Raphael(options.id,this.width,this.height);
+	this.setGrid();
+	this.setGraph();
 
-	},
+}
 
-	setGrid: function(){
+chartphael.line.prototype.setGrid = function(){
 
-		var path = null;
-		var incrementX = (this.bottom_right[0] - this.bottom_left[0])/(this.x_axis_value.length-1);
-		var incrementY = (this.bottom_left[1] - this.top_left[1])/(this.y_axis_value.length-1);
-		var textAttr = {fill: '#666','font-size':'10px'};
+	var path = null;
+	var textAttr = {fill: '#666','font-size':'10px'};
 
-		for (i=0;i<this.y_axis_value.length;i++){
-			var currInc = incrementY*i;
-			if(i!=0&&i!=this.y_axis_value.length-1){
-				path += 'M'+ this.bottom_left[0] +' '+ (this.bottom_left[1]-currInc) +'L'+ this.bottom_right[0] +' '+ (this.bottom_right[1]-currInc);
-			}
-			this.paper.text(this.bottom_left[0]-this.padding/2, this.bottom_left[1]-currInc, this.y_axis_value[i]).attr(textAttr);
+	for (i=0;i<this.y_axis_value.length;i++){
+		var currInc = this.incrementY*i;
+		if(i!=0&&i!=this.y_axis_value.length-1){
+			path += 'M'+ this.bottom_left[0] +' '+ (this.bottom_left[1]-currInc) +'L'+ this.bottom_right[0] +' '+ (this.bottom_right[1]-currInc);
 		}
-
-		for (i=0;i<this.x_axis_value.length;i++){
-			var currInc = incrementX*i;
-			if(i!=0&&i!=this.x_axis_value.length-1){
-				path += 'M'+ (this.bottom_left[0]+currInc) +' '+ this.bottom_left[1] +'L'+ (this.top_left[0]+currInc) +' '+ this.top_left[1];	
-			}
-			this.paper.text(this.bottom_left[0]+currInc, this.bottom_left[1]+this.padding/2, this.x_axis_value[i]).attr(textAttr);
-		}
-
-		this.paper.path(path).attr({stroke: '#d8decf', 'stroke-width': 1});
-
-	},
-
-	setPoints: function(){
-
+		this.paper.text(this.bottom_left[0]-this.padding/2, this.bottom_left[1]-currInc, this.y_axis_value[i]).attr(textAttr);
 	}
+
+	for (i=0;i<this.x_axis_value.length;i++){
+		var currInc = this.incrementX*i;
+		if(i!=0&&i!=this.x_axis_value.length-1){
+			path += 'M'+ (this.bottom_left[0]+currInc) +' '+ this.bottom_left[1] +'L'+ (this.top_left[0]+currInc) +' '+ this.top_left[1];	
+		}
+		this.paper.text(this.bottom_left[0]+currInc, this.bottom_left[1]+this.padding/2, this.x_axis_value[i]).attr(textAttr);
+	}
+
+	this.grid = this.paper.path(path).attr({stroke: '#d8decf', 'stroke-width': 1});
+
+};
+
+chartphael.line.prototype.setGraph = function(){
+
+	var items = this.data.items,
+		circleAttr = {fill: '#829c27', 'stroke-width': 2, stroke: '#fff'},
+		dataWidth = 24,
+		dataHeight = 175,
+		path = null;
+
+	for(i=0;i<items.length;i++){
+		var pointPosX = this.bottom_left[0] + (items[i].x*(this.gridWidth/dataWidth)),
+			pointPosY = this.bottom_left[1] - (items[i].y*(this.gridHeight/dataHeight));
+		this.paper.circle(pointPosX, pointPosY, 6).attr(circleAttr);
+		if(i==0){
+			path += 'M'+ pointPosX +' '+ pointPosY;
+		} else {
+			path += 'L'+ pointPosX +' '+ pointPosY;
+		}
+	}
+
+	this.paper.path(path).attr({stroke: '#829c27', 'stroke-width': 4}).toBack();
+	this.paper.path(path+'L'+ this.bottom_right[0] +' '+ this.bottom_right[1] +'L'+ this.bottom_left[0] +' '+ this.bottom_left[1]).attr({fill: 'rgba(130,156,39,20)', 'stroke-width': 0}).toBack();
+	this.grid.toBack();
 
 };
